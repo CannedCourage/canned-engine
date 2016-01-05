85370459d0efe1f79cb2938d2fcb504807046019
Scott Oliver <scott@scottoliverdev.com>
2016-01-05 07:36:14 +0000

    Removed scrap files
    I had kept previous versions of various bits of code in a "scrap" folder
    before the project was in source control. These files will no longer be
    tracked.

--- Scrap/Clamp.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CLAMP_H_
-#define _CLAMP_H_
-
-int clamp( const int value, const int min, const int max );
-float clamp( const float value, const float min, const float max );
-
-#endif //_CLAMP_H_
--- Scrap/Metrics.h
+++
@@ -1,1 +0,0 @@
-#define PLAYERSPEED 0.1f
