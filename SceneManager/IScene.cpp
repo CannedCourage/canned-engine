e613d9c62b39c9835771dadc221bdcaf9f2ce91d
Scott Oliver <scott@scottoliverdev.com>
2016-04-04 21:20:14 +0100

    Qualifying scope due to switch to scoped enum
    A change to the Priority enum to be a scoped enum requires all uses of
    the enum values to be qualified with the enum name.

--- SceneManager/IScene.cpp
+++ SceneManager/IScene.cpp
@@ -2,19 +2,15 @@
 #include "SceneManager/IScene.h"
 #include "System/System.h"
 
-IScene::IScene( System &sys, const char* n ) : 	system( sys ),
-												manager( sys.sceneManager ),
-												settings( sys.settings ),
-												graphics( sys.graphics ),
-												input( sys.input ),
-												sound( sys.sound ),
-												assets( sys.assets ),
-												entityManager(),
-												engine(),
-												log( n ),
-												name( n ),
-												loaded( false ),
-												lost( false )
+IScene::IScene( System &sys, const std::string& N ) : 	system( sys ),
+														manager( sys.sceneManager ),
+														settings( sys.settings ),
+														graphics( sys.graphics ),
+														input( sys.input ),
+														sound( sys.sound ),
+														assets( sys.assets ),
+														log( N ),
+														Name( N )
 {
 }
 
@@ -24,37 +20,37 @@ IScene::~IScene( void )
 
 void IScene::PreUpdate( void )
 {
-	engine.UpdateProcesses( system.time.DeltaTime(), PRE_UPDATE );
+	engine.UpdateProcesses( system.time.DeltaTime(), Priority::PRE_UPDATE );
 }
 
 void IScene::Update( void )
 {
-	engine.UpdateProcesses( system.time.DeltaTime(), UPDATE );
+	engine.UpdateProcesses( system.time.DeltaTime(), Priority::UPDATE );
 }
 
 void IScene::FixedUpdate( void )
 {
-	engine.UpdateProcesses( system.time.DeltaTime(), FIXED_UPDATE );
+	engine.UpdateProcesses( system.time.DeltaTime(), Priority::FIXED_UPDATE );
 }
 
 void IScene::PostUpdate( void )
 {
-	engine.UpdateProcesses( system.time.DeltaTime(), POST_UPDATE );
+	engine.UpdateProcesses( system.time.DeltaTime(), Priority::POST_UPDATE );
 }
 
 void IScene::PreRender( void )
 {
-	engine.UpdateProcesses( system.time.DeltaTime(), PRE_RENDER );
+	engine.UpdateProcesses( system.time.DeltaTime(), Priority::PRE_RENDER );
 }
 
 void IScene::Render( void )
 {
-	engine.UpdateProcesses( system.time.DeltaTime(), RENDER );
+	engine.UpdateProcesses( system.time.DeltaTime(), Priority::RENDER );
 }
 
 void IScene::PostRender( void )
 {
-	engine.UpdateProcesses( system.time.DeltaTime(), POST_RENDER );
+	engine.UpdateProcesses( system.time.DeltaTime(), Priority::POST_RENDER );
 }
 
 inline const bool IScene::IsLoaded( void ) const
