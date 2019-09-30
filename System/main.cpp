367fb5e120e5a67d3fa2ff8550b870b51ba1836a
scott oliver <scott.oliver@menziesdistribution.com>
2019-09-30 13:25:36 +0100

    Moved callback setup from System class to main

--- System/main.cpp
+++ System/main.cpp
@@ -9,6 +9,9 @@
 
 #include "Graphics/DisableSteamOverlay.h"
 
+void SetupInputCallbacks( WindowGLFW& window, Input& input );
+void SetupRenderCallbacks( WindowGLFW& window, GraphicsVK& graphics );
+
 int main()
 {
     std::unique_ptr<System> sys;
@@ -21,7 +24,9 @@ int main()
         GraphicsVK graphics{ window.GetWindow() };
         Sound sound{};
         Input input{};
-        
+
+        SetupInputCallbacks( window, input );
+
         HandleWin32(); //TODO: Rename this to HandlePlatformSpecific?
 
         sys = std::make_unique<System>( window, graphics, sound, input );
@@ -41,4 +46,54 @@ int main()
     }
 
     return result;
+}
+
+void SetupInputCallbacks( WindowGLFW& window, Input& input )
+{
+    auto key_callback = [&input]( GLFWwindow* Window, int Key, int Scancode, int Action, int Mods )
+    {
+        input.ReceiveKeyboardInput( Key, Scancode, Action, Mods );
+    };
+
+    auto cursor_position_callback = [&input]( GLFWwindow* Window, double X, double Y )
+    {
+        input.ReceiveMousePosition( X, Y );
+    }
+
+    auto mouse_button_callback = [&input]( GLFWwindow* Window, int Button, int Action, int Mods )
+    {
+        input.ReceiveMouseInput( Button, Action, Mods );
+    }
+
+    auto scroll_callback = [&input]( GLFWwindow* Window, double XOffset, double YOffset )
+    {
+        input.ReceiveScrollInput( XOffset, YOffset );
+    }
+
+    auto scroll_callback = [&input]( GLFWwindow* Window, double XOffset, double YOffset )
+    {
+        input.ReceiveScrollInput( XOffset, YOffset );
+    }
+
+    glfwSetKeyCallback( window.GetWindow(), key_callback );
+    glfwSetCursorPosCallback( window.GetWindow(), cursor_position_callback );
+    glfwSetMouseButtonCallback( window.GetWindow(), mouse_button_callback );
+    glfwSetScrollCallback( window.GetWindow(), scroll_callback );
+}
+
+void SetupRenderCallbacks( WindowGLFW& window, GraphicsVK& graphics )
+{
+    auto window_size_callback = [&graphics]( GLFWwindow* Window, int Width, int Height )
+    {
+        //Record in settings?
+    }
+
+    auto framebuffer_size_callback = [&graphics]( GLFWwindow* Window, int Width, int Height )
+    {
+        //Update graphics service + record in settings
+        //glViewport(0, 0, width, height);
+    }
+
+    glfwSetWindowSizeCallback( window.GetWindow(), window_size_callback );
+    glfwSetFramebufferSizeCallback( window.GetWindow(), framebuffer_size_callback );
 }
